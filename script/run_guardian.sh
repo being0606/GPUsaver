dd3d36e9d6d32823d145926be445e49cf2e92262
bein0606 <onuhjin@gmail.com>
2025-10-08 02:48:45 +0900

    feat: add machine-name & notification_state arg

---
+++ script/run_guardian.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 이 스크립트는 call_guardian.py를 실행하고 모든 명령줄 인자를 전달합니다.
+# 사용 예시:
+# 1. 기본값(A6000)으로 실행:
+#    ./script/run_guardian.sh
+# 2. 머신 이름 'H100'으로 지정하여 실행:
+#    ./script/run_guardian.sh --machine-name H100
+
+echo "Starting GPU Guardian..."
+python src/call_guardian.py --machine-name A6000
